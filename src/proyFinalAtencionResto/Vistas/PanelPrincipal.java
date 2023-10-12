724b86b528e4118716a6971ecb684c67194b03c7
daniel-calvo <danielcst87@gmail.com>
2023-10-12 18:33:03 -0300

    Vista ListaPedidosMesero

--- src/proyFinalAtencionResto/Vistas/PanelPrincipal.java
+++ src/proyFinalAtencionResto/Vistas/PanelPrincipal.java
@@ -45,6 +45,7 @@ public class PanelPrincipal extends javax.swing.JFrame {
         jMenu4 = new javax.swing.JMenu();
         jmCobrarPedido = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jmSalir = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
 
@@ -146,6 +147,15 @@ public class PanelPrincipal extends javax.swing.JFrame {
         jMenuBar1.add(jMenu4);
 
         jMenu3.setText("Listados");
+
+        jMenuItem1.setText("ListaDePedidosPorMesero");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem1);
+
         jMenuBar1.add(jMenu3);
 
         jmSalir.setText("Salir");
@@ -241,24 +251,35 @@ public class PanelPrincipal extends javax.swing.JFrame {
 
     private void jmGestionProductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmGestionProductosActionPerformed
         // TODO add your handling code here:
-        escritorio.removeAll();
-        escritorio.repaint();
-        GestionProductos x = new GestionProductos();
-        x.setVisible(true);
-        escritorio.add(x);
-        escritorio.moveToFront(x);
+//        escritorio.removeAll();
+//        escritorio.repaint();
+//        GestionProductos x = new GestionProductos();
+//        x.setVisible(true);
+//        escritorio.add(x);
+//        escritorio.moveToFront(x);
     }//GEN-LAST:event_jmGestionProductosActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+//        escritorio.removeAll();
+//        escritorio.repaint();
+//        GestionProductos x = new GestionProductos();
+//        x.setSize(1100,613);
+//        x.setVisible(true);
+//        escritorio.add(x);
+//        escritorio.moveToFront(x);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
         escritorio.removeAll();
         escritorio.repaint();
-        GestionProductos x = new GestionProductos();
+        ListaPedidosMesero x = new ListaPedidosMesero();
         x.setSize(1100,613);
         x.setVisible(true);
         escritorio.add(x);
         escritorio.moveToFront(x);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -302,6 +323,7 @@ public class PanelPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jmAlta;
     private javax.swing.JMenuItem jmBaja;
--- src/proyFinalAtencionResto/Vistas/Principal_main.java
+++ src/proyFinalAtencionResto/Vistas/Principal_main.java
@@ -2,10 +2,14 @@
 package proyFinalAtencionResto.Vistas;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 import proyFinalAtencionResto.AccesoADatos.Coneccion;
+import proyFinalAtencionResto.AccesoADatos.MeseroData;
 import proyFinalAtencionResto.AccesoADatos.ProductoData;
+import proyFinalAtencionResto.Entidades.Mesero;
 import proyFinalAtencionResto.Entidades.Producto;
 
 
@@ -22,9 +26,14 @@ public class Principal_main {
     
     public static void main(String[] args) {
     
-    PanelPrincipal menu = new PanelPrincipal();
-    menu.setVisible(true);
-    
+//    PanelPrincipal menu = new PanelPrincipal();
+//    menu.setVisible(true);
+    MeseroData md = new MeseroData();
+    List<Mesero> meseros = new ArrayList<>();
+    meseros = md.listaDeMeseros();
+    for(Mesero mesero: meseros) {
+        System.out.println(mesero);
+    }
        
 
     }
